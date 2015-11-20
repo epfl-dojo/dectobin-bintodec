023465524dd9f60933ecad7d274f1622a334b9b8
Loïc Humbert <loic.humbert@epfl.ch>
2015-11-20 13:54:36 +0100

    dectobin fonctionne

--- dectobin.cpp
+++ dectobin.cpp
@@ -12,7 +12,7 @@ int dectobin(int, int[16]);
 void main(void)
 {
 	int input;
-	int binVal[16] = { 0 };
+	int binVal[16] = {0};
 	int decVal;
 
 	cout << "Tappez 1 pour convertir un dec en bin ou tappez 0 pour convertir un bin en dec" << endl;
@@ -26,7 +26,7 @@ void main(void)
 		case 1:
 			cout << "Tappez un dec a convertir en binaire: " << endl;
 			cin >> input;
-			cout << dectobin(input, binVal);
+			dectobin(input, binVal);
 			break;
 		default:
 			cout << "AARRRGGGHH!" << endl;
@@ -38,15 +38,20 @@ void main(void)
 int dectobin(int input, int binVal[])
 {
 	int index = 0;
-	cout << input;
-	while (input > 0)
+	while (input != 0)
 	{
 		int bit = input % 2;
-		binVal[index] = 1;
+		binVal[index] = bit;
 		input /= 2;
 		index++;
-		cout << sizeof(binVal)/sizeof(int) << endl;
+		//cout << sizeof(binVal)/sizeof(int) << endl;
 	}
-	cout << binVal[0];
-	return input;
+	index--;
+	while (index >= 0)
+	{
+		cout << binVal[index];
+		index--;
+	}
+	cout << endl;
+	return binVal[index];
 }
